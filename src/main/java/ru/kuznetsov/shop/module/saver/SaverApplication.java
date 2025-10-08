35bf4dde5ddc227856296d7002f4d08cbd8d7598
Pavel Kuznetsov <Technic47@gmail.com>
2025-10-08 15:45:15 +0300

    Init commit.

---
+++ src/main/java/ru/kuznetsov/shop/module/saver/SaverApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.module.saver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaverApplication.class, args);
+    }
+
+}
