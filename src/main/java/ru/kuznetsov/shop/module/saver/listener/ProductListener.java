63edf968c346e630aee3a296a743a2c583208b3d
Pavel Kuznetsov <Technic47@gmail.com>
2025-10-13 13:39:51 +0300

    Deserialisation fixed.

--- src/main/java/ru/kuznetsov/shop/module/saver/listener/ProductListener.java
+++ src/main/java/ru/kuznetsov/shop/module/saver/listener/ProductListener.java
@@ -1,5 +1,7 @@
 package ru.kuznetsov.shop.module.saver.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +17,14 @@ import static ru.kuznetsov.shop.data.common.KafkaTopics.PRODUCT_SAVE_TOPIC;
 public class ProductListener {
 
     private final ProductService productService;
+    private final ObjectMapper objectMapper;
 
     Logger logger = LoggerFactory.getLogger(ProductListener.class);
 
     @KafkaListener(topics = PRODUCT_SAVE_TOPIC, groupId = "${spring.kafka.consumer.group-id}")
-    public void saveProduct(ProductDto productDto) {
+    public void saveProduct(String productDto) throws JsonProcessingException {
         logger.info("Saving product {}", productDto);
-        productService.add(productDto);
+        productService.add(objectMapper.readValue(productDto, ProductDto.class));
+        logger.info("Product Saved");
     }
 }
